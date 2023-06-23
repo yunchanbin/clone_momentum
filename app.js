326b9d98fafcb91722458a258521aed7dd77bea9
yunchanbin <21611273@yu.ac.kr>
2023-06-24 00:26:38 +0900

    2.5

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const amIFat = null;
-let someting;
 
-console.log(someting);
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
