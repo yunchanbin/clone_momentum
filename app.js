39cbd1609ed9a21a34c0dfbef80ba2c15d4ab0e1
yunchanbin <21611273@yu.ac.kr>
2023-06-26 00:10:09 +0900

    2.10-1

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const age = 96;
-function calculateKrAge(ageOfForeigner){
-    ageOfForeigner + 2;
-
+const calculator = {
+    add:function(a, b){
+        return a + b;
+    },
+    minus:function(a, b){
+        return a - b;
+    },
+    times:function(a,b){
+        return a * b;
+    },
+    divide:function(a, b){
+        return a / b;
+    },
+    power:function(a, b){
+        return a ** b;
+    }
 }
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
 
 
+const plusResult = calculator.add(2, 4);
+calculator.minus(2, 4);
+calculator.times(2, 4);
+calculator.divide(2, 4);
+calculator.power(2, 4);
+
+
+console.log(plusResult);
