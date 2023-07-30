20d6a62c87f9a47d45dce9348cafdb436d4920c5
yunchanbin <21611273@yu.ac.kr>
2023-07-30 19:39:52 +0900

    2.11

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-const calculator = {
-    add:function(a, b){
-        return a + b;
-    },
-    minus:function(a, b){
-        return a - b;
-    },
-    times:function(a,b){
-        return a * b;
-    },
-    divide:function(a, b){
-        return a / b;
-    },
-    power:function(a, b){
-        return a ** b;
-    }
-}
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.add(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
-
-console.log(plusResult);
-console.log(powerResult);
+console.log(age);
+console.log(typeof age);
