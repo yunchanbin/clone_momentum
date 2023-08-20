50fd8aa64ef3b279d4aa691e74d46a27dfa3fc6e
yunchanbin <21611273@yu.ac.kr>
2023-08-21 04:07:31 +0900

    7.1

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,24 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#tode-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
+
+  toDoList.appendChild(li);
+  li.appendChild(span);
+  li.appendChild(button);
+  span.innerText = newTodo;
+  
+}
+
 function handleToDoSubmit(event){
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
